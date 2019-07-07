b5fb2af9caf6b3db7dd6a817bc1a476a1df57153
1171430615 <752497070@qq.com>
2019-07-07 19:22:25 +0800

    1171430615

--- test.py
+++ test.py
@@ -2,3 +2,7 @@ for i in range(10):
     for j in range(i):
         print("Hello ljr ",end="")
     print()
+for i in range(10):
+    for j in range(10-i):
+        print("Hello ljr ",end="")
+    print()
